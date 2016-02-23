ec7a65bd1f31abeeffd96faa8331321067c7f4fa
kydos <angelo@icorsaro.net>
2016-02-23 14:25:29 +0100

    fixed class loading issue on windows

--- agentv/src/main/scala/com/prismtech/agentv/Repository.scala
+++ agentv/src/main/scala/com/prismtech/agentv/Repository.scala
@@ -64,7 +64,7 @@ class Repository (val path: String) {
 
 
   def resolveClass(s: String): Option[Array[Byte]] = {
-    val n = s.replace(".", File.separator) + ".class"
+    val n = s.replace(".", "/") + ".class"
     jars.get().map(jar => (jar, jar.getEntry(n))).filter(p => {
       logger.debug(p.toString())
       p._2 != null
